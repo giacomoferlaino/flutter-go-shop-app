77b1b1c34c33bd43cfc331eb10235517b1b1df75
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-06-30 18:44:02 +0200

    feat: Improved application styling

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,25 +9,30 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: () {},
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
         ),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: () {},
+        child: Image.network(
+          product.imageUrl,
+          fit: BoxFit.cover,
         ),
-        title: Text(
-          product.title,
-          textAlign: TextAlign.center,
-        ),
-      ),
-      child: Image.network(
-        product.imageUrl,
-        fit: BoxFit.cover,
       ),
     );
   }
