886ae3695fe4fcfd74c55a0cfdaba976163850f6
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-07-09 16:45:12 +0200

    feat: Added product POST method

--- back-end/product/handler.go
+++ back-end/product/handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flutter_shop_app/app"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -19,13 +20,14 @@ type Handler struct {
 	app app.State
 }
 
-// Get handles the HTTP GET method
+// Get returns all saved products
 func (handler *Handler) Get(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	query := "select * from product"
 	rows, err := handler.app.Database.Query(query)
 	if err != nil {
 		res.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(res, err)
+		return
 	}
 	defer rows.Close()
 
@@ -33,10 +35,11 @@ func (handler *Handler) Get(res http.ResponseWriter, req *http.Request, _ httpro
 
 	for rows.Next() {
 		product := Product{}
-		err := rows.Scan(&product)
+		err := rows.Scan(&product.ID, &product.Title, &product.Description, &product.Price, &product.ImageURL, &product.IsFavorite)
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprint(res, err)
+			return
 		}
 		products = append(products, product)
 
@@ -45,6 +48,42 @@ func (handler *Handler) Get(res http.ResponseWriter, req *http.Request, _ httpro
 	if err != nil {
 		res.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(res, err)
+		return
 	}
+	res.WriteHeader(http.StatusOK)
 	fmt.Fprintln(res, string(data))
+}
+
+// Post saves a new product
+func (handler *Handler) Post(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	reqBody, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(res, err)
+		return
+	}
+	newProduct := &Product{}
+	err = json.Unmarshal(reqBody, newProduct)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(res, err)
+		return
+	}
+	query := `INSERT INTO product (title, description, price, imageUrl, isFavorite)
+	VALUES(?, ?, ?, ?, ?)`
+	result, err := handler.app.Database.Exec(query, newProduct.Title, newProduct.Description, newProduct.Price, newProduct.ImageURL, newProduct.IsFavorite)
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(res, err)
+		return
+	}
+	res.WriteHeader(http.StatusOK)
+	newID, err := result.LastInsertId()
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(res, err)
+		return
+	}
+	fmt.Fprintln(res, newID)
 }
