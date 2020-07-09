df61c578dce64e53af93660ea497cf00731cefb4
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-07-09 15:51:34 +0200

    feat: Added product data model

--- back-end/product/product.go
+++ back-end/product/product.go
@@ -1,24 +1,11 @@
 package product
 
-import (
-	"flutter_shop_app/app"
-	"fmt"
-	"net/http"
-
-	"github.com/julienschmidt/httprouter"
-)
-
-// NewHandler returns a new Handler
-func NewHandler(app app.State) *Handler {
-	return &Handler{app: app}
-}
-
-// Handler contains the HTTP endpoint handlers
-type Handler struct {
-	app app.State
-}
-
-// Get handles the HTTP GET method
-func (handler *Handler) Get(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	fmt.Fprint(res, "Index page!")
+// Product data model
+type Product struct {
+	ID          string  `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	ImageURL    string  `json:"imageUrl"`
+	IsFavorite  bool    `json:"isFavorite"`
 }
