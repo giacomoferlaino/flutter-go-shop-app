6c2d9e09b523c8eb1665d7bb912bb6a0fec58aed
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-07-10 19:51:58 +0200

    feat: Added ProductService getAll method

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -19,4 +19,19 @@ class ProductService {
     final String id = json.decode(response.body)['id'];
     return id;
   }
+
+  Future<List<Product>> getAll() async {
+    Response response = await get(_fullPath);
+    List<Map<String, Object>> products = json.decode(response.body);
+    return products.map(
+      (item) => Product(
+        id: item['id'],
+        description: item['description'],
+        imageUrl: item['imageUrl'],
+        price: item['price'],
+        title: item['title'],
+        isFavorite: item['isFavorite'],
+      ),
+    );
+  }
 }
