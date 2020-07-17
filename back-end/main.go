02868c741c022f8d2b378a962ed40d1ff7d77ad8
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-07-17 11:38:59 +0200

    feat: Registered /order api endpoint routes

--- back-end/main.go
+++ back-end/main.go
@@ -22,12 +22,19 @@ func main() {
 		Database: db,
 	}
 	productHandler := NewHandler(app, &orm.ProductDataStore{DB: app.Database})
+	orderHandler := NewHandler(app, &orm.OrderDataStore{DB: app.Database})
 	router := httprouter.New()
 	router.GET("/product", productHandler.Get)
 	router.POST("/product", productHandler.Post)
 	router.GET("/product/:id", productHandler.GetByID)
 	router.PUT("/product/:id", productHandler.UpdateByID)
 	router.DELETE("/product/:id", productHandler.DeleteByID)
+
+	router.GET("/order", orderHandler.Get)
+	router.POST("/order", orderHandler.Post)
+	router.GET("/order/:id", orderHandler.GetByID)
+	router.PUT("/order/:id", orderHandler.UpdateByID)
+	router.DELETE("/order/:id", orderHandler.DeleteByID)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
