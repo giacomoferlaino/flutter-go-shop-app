2b531069cf540e082683a68456f4ec98a6522ad5
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-07-17 11:37:02 +0200

    feat: Added ParseJSON method to DataStore interface

--- back-end/orm/data_store.go
+++ back-end/orm/data_store.go
@@ -1,7 +1,10 @@
 package orm
 
+import "io"
+
 // DataStore contains the methods that all the data store has to implement
 type DataStore interface {
+	ParseJSON(reqBody io.ReadCloser) (interface{}, error)
 	GetAll() (*Response, error)
 	GetByID(id uint) (*Response, error)
 	Add(item interface{}) (*Response, error)
