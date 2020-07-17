ceff09c03a71e825b65d3221fec9ae597b9cda50
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-07-17 11:36:44 +0200

    feat: Generalized Handler type to depend on the DataStore interface

--- back-end/handler.go
+++ back-end/handler.go
@@ -5,8 +5,6 @@ import (
 	"flutter_shop_app/app"
 	"flutter_shop_app/orm"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 
@@ -24,21 +22,7 @@ type Handler struct {
 	store orm.DataStore
 }
 
-func (handler *Handler) parseProduct(reqBody io.ReadCloser) (*orm.Product, error) {
-	body, err := ioutil.ReadAll(reqBody)
-	defer reqBody.Close()
-	if err != nil {
-		return nil, err
-	}
-	product := &orm.Product{}
-	err = json.Unmarshal(body, product)
-	if err != nil {
-		return nil, err
-	}
-	return product, nil
-}
-
-// Get returns all saved products
+// Get returns all saved items
 func (handler *Handler) Get(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	queryResponse, err := handler.store.GetAll()
 	if err != nil {
@@ -56,15 +40,15 @@ func (handler *Handler) Get(res http.ResponseWriter, req *http.Request, _ httpro
 	fmt.Fprintln(res, string(apiResponse))
 }
 
-// Post saves a new product
+// Post saves a new item
 func (handler *Handler) Post(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-	newProduct, err := handler.parseProduct(req.Body)
+	newItem, err := handler.store.ParseJSON(req.Body)
 	if err != nil {
 		res.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(res, err)
 		return
 	}
-	queryResponse, err := handler.store.Add(*newProduct)
+	queryResponse, err := handler.store.Add(newItem)
 	if err != nil {
 		res.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(res, err)
@@ -80,7 +64,7 @@ func (handler *Handler) Post(res http.ResponseWriter, req *http.Request, _ httpr
 	fmt.Fprintln(res, string(apiResponse))
 }
 
-// GetByID returns a product based on its ID
+// GetByID returns a item based on its ID
 func (handler *Handler) GetByID(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	id, err := strconv.ParseUint(params.ByName("id"), 10, 64)
 	if err != nil {
@@ -104,7 +88,7 @@ func (handler *Handler) GetByID(res http.ResponseWriter, req *http.Request, para
 	fmt.Fprintln(res, string(apiResponse))
 }
 
-// DeleteByID returns the number of deleted rows
+// DeleteByID delete an item based on its ID
 func (handler *Handler) DeleteByID(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil {
@@ -128,7 +112,7 @@ func (handler *Handler) DeleteByID(res http.ResponseWriter, req *http.Request, p
 	fmt.Fprintln(res, string(apiResponse))
 }
 
-// UpdateByID returns the number of deleted rows
+// UpdateByID update an items based on its ID
 func (handler *Handler) UpdateByID(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 	if err != nil {
@@ -136,13 +120,13 @@ func (handler *Handler) UpdateByID(res http.ResponseWriter, req *http.Request, p
 		fmt.Fprint(res, err)
 		return
 	}
-	product, err := handler.parseProduct(req.Body)
+	item, err := handler.store.ParseJSON(req.Body)
 	if err != nil {
 		res.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(res, err)
 		return
 	}
-	queryResponse, err := handler.store.UpdateByID(uint(id), *product)
+	queryResponse, err := handler.store.UpdateByID(uint(id), item)
 	if err != nil {
 		res.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprint(res, err)
