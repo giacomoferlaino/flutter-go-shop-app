863df91abffa01de6f193ceedf5e140fbe1edb7f
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-07-26 20:49:46 +0200

    feat: Added Auth provider to global providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
-import 'package:shop_app/pages/auth_page.dart';
 
 import './providers/cart.dart';
 import './providers/products.dart';
 import './providers/orders.dart';
 import './providers/shop_filters.dart';
+import './providers/auth.dart';
 import './pages/orders_page.dart';
 import './pages/cart_page.dart';
 import './pages/products_overview_page.dart';
 import './pages/product_detailt_page.dart';
 import './pages/user_products_page.dart';
 import './pages/edit_product_page.dart';
+import './pages/auth_page.dart';
 import './services/product_service.dart';
 import './services/snack_bar_service.dart';
 import './services/order_service.dart';
+import './services/auth_service.dart';
 
 const String baseUrl = 'http://10.0.2.2:8080';
 
@@ -23,6 +25,7 @@ void serviceLocatorSetup() {
   GetIt serviceLocator = GetIt.instance;
   serviceLocator.registerSingleton<ProductService>(ProductService(baseUrl));
   serviceLocator.registerSingleton<OrderService>(OrderService(baseUrl));
+  serviceLocator.registerSingleton<AuthService>(AuthService(baseUrl));
   serviceLocator.registerSingleton<SnackBarService>(
     SnackBarService(
       duration: Duration(seconds: 2),
@@ -40,6 +43,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(
+          create: (context) => Auth(),
+        ),
         ChangeNotifierProvider(
           create: (context) => Products(),
         ),
