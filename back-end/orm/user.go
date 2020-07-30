04b8e5bb800247d702f9fb2f38c5a9461523a01d
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-07-31 00:21:39 +0200

    feat: Updated user data model to include Product and FavoriteProducts relations

--- back-end/orm/user.go
+++ back-end/orm/user.go
@@ -3,8 +3,10 @@ package orm
 // User data model
 type User struct {
 	Model
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email" gorm:"unique"`
-	Password  string `json:"password"`
+	FirstName        string    `json:"firstName"`
+	LastName         string    `json:"lastName"`
+	Email            string    `json:"email" gorm:"unique"`
+	Password         string    `json:"password"`
+	Products         []Product `json:"products" gorm:"many2many:users_products"`
+	FavoriteProducts []Product `json:"favoriteProducts" gorm:"many2many:users_favoriteProducts"`
 }
