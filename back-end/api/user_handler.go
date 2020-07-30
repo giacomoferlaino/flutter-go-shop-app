03dcc9b1709be640be882c1f2f2f4332789823c2
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-07-31 00:43:20 +0200

    feat: Added APIs to get all products and favorite products for the logged in user

--- back-end/api/user_handler.go
+++ back-end/api/user_handler.go
@@ -47,66 +47,110 @@ func (handler *UserHandler) ServeHTTP(res http.ResponseWriter, req *http.Request
 		sendError(res, err)
 		return
 	}
+
+	route := strings.Split(req.URL.Path, "/")[2]
+	switch route {
+	case userRoutes.Product:
+		switch req.Method {
+		case method.GET:
+			handler.GetProducts(res, req, userID)
+		case method.POST:
+			handler.AddProduct(res, req, userID)
+		case method.DELETE:
+			handler.RemoveProduct(res, req, userID)
+		}
+	case userRoutes.FavoriteProduct:
+		switch req.Method {
+		case method.GET:
+			handler.GetFavoriteProducts(res, req, userID)
+		case method.POST:
+			handler.AddFavoriteProduct(res, req, userID)
+		case method.DELETE:
+			handler.RemoveFavoriteProduct(res, req, userID)
+		}
+	}
+}
+
+// GetProducts reads all the product related to a specific user
+func (handler *UserHandler) GetProducts(res http.ResponseWriter, req *http.Request, userID uint) {
+	data, _, err := handler.store.GetByID(userID, true)
+	if err != nil {
+		sendError(res, err)
+		return
+	}
+	users := data.([]orm.User)
+	products := users[0].Products
+	sendSuccess(res, products, int64(len(products)))
+}
+
+// AddProduct adds a new product to a given user
+func (handler *UserHandler) AddProduct(res http.ResponseWriter, req *http.Request, userID uint) {
 	productID, err := strconv.ParseUint(req.FormValue("productId"), 10, 64)
 	if err != nil {
 		sendError(res, errors.New("Invalid product id"))
 		return
 	}
-	route := strings.Split(req.URL.Path, "/")[2]
-	switch route {
-	case userRoutes.Product:
-		switch req.Method {
-		case method.POST:
-			handler.AddProduct(res, req, userID, uint(productID))
-		case method.DELETE:
-			handler.RemoveProduct(res, req, userID, uint(productID))
-		}
-	case userRoutes.FavoriteProduct:
-		switch req.Method {
-		case method.POST:
-			handler.AddFavoriteProduct(res, req, userID, uint(productID))
-		case method.DELETE:
-			handler.RemoveFavoriteProduct(res, req, userID, uint(productID))
-		}
-	}
-}
-
-// AddProduct adds a new product to a given user
-func (handler *UserHandler) AddProduct(res http.ResponseWriter, req *http.Request, userID uint, productID uint) {
-	err := handler.store.AddProduct(userID, productID)
+	err = handler.store.AddProduct(userID, uint(productID))
 	if err != nil {
 		sendError(res, err)
 		return
 	}
-	sendSuccess(res, struct{}{}, 0)
+	sendSuccess(res, []struct{}{}, 0)
 }
 
 // RemoveProduct removes a product from a give user
-func (handler *UserHandler) RemoveProduct(res http.ResponseWriter, req *http.Request, userID uint, productID uint) {
-	err := handler.store.RemoveProduct(userID, productID)
+func (handler *UserHandler) RemoveProduct(res http.ResponseWriter, req *http.Request, userID uint) {
+	productID, err := strconv.ParseUint(req.FormValue("productId"), 10, 64)
+	if err != nil {
+		sendError(res, errors.New("Invalid product id"))
+		return
+	}
+	err = handler.store.RemoveProduct(userID, uint(productID))
 	if err != nil {
 		sendError(res, err)
 		return
 	}
-	sendSuccess(res, struct{}{}, 0)
+	sendSuccess(res, []struct{}{}, 0)
+}
+
+// GetFavoriteProducts reads all the product related to a specific user
+func (handler *UserHandler) GetFavoriteProducts(res http.ResponseWriter, req *http.Request, userID uint) {
+	data, _, err := handler.store.GetByID(userID, true)
+	if err != nil {
+		sendError(res, err)
+		return
+	}
+	users := data.([]orm.User)
+	products := users[0].FavoriteProducts
+	sendSuccess(res, products, int64(len(products)))
 }
 
 // AddFavoriteProduct adds a new favorite product to a given user
-func (handler *UserHandler) AddFavoriteProduct(res http.ResponseWriter, req *http.Request, userID uint, productID uint) {
-	err := handler.store.AddFavoriteProduct(userID, productID)
+func (handler *UserHandler) AddFavoriteProduct(res http.ResponseWriter, req *http.Request, userID uint) {
+	productID, err := strconv.ParseUint(req.FormValue("productId"), 10, 64)
+	if err != nil {
+		sendError(res, errors.New("Invalid product id"))
+		return
+	}
+	err = handler.store.AddFavoriteProduct(userID, uint(productID))
 	if err != nil {
 		sendError(res, err)
 		return
 	}
-	sendSuccess(res, struct{}{}, 0)
+	sendSuccess(res, []struct{}{}, 0)
 }
 
 // RemoveFavoriteProduct removes a favorite product from a give user
-func (handler *UserHandler) RemoveFavoriteProduct(res http.ResponseWriter, req *http.Request, userID uint, productID uint) {
-	err := handler.store.RemoveFavoriteProduct(userID, productID)
+func (handler *UserHandler) RemoveFavoriteProduct(res http.ResponseWriter, req *http.Request, userID uint) {
+	productID, err := strconv.ParseUint(req.FormValue("productId"), 10, 64)
+	if err != nil {
+		sendError(res, errors.New("Invalid product id"))
+		return
+	}
+	err = handler.store.RemoveFavoriteProduct(userID, uint(productID))
 	if err != nil {
 		sendError(res, err)
 		return
 	}
-	sendSuccess(res, struct{}{}, 0)
+	sendSuccess(res, []struct{}{}, 0)
 }
