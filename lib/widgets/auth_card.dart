68db61f39bcec4a347c9116618eec51d99cac494
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-08-07 16:11:05 +0200

    feat: Using AnimatedBuilder to improve performance

--- lib/widgets/auth_card.dart
+++ lib/widgets/auth_card.dart
@@ -42,7 +42,6 @@ class _AuthCardState extends State<AuthCard>
       parent: _animationController,
       curve: Curves.fastOutSlowIn,
     ));
-    _heightAnimation.addListener(() => setState(() {}));
   }
 
   void _submit() async {
@@ -93,11 +92,15 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (context, child) => Container(
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: child,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
