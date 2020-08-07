7407e31be0efddcf346cff5f0fd19579ab62009c
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-08-07 16:54:33 +0200

    feat: Added Hero animation on product detail navigation

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -65,10 +65,14 @@ class ProductItem extends StatelessWidget {
               Navigator.of(context).pushNamed(ProductDetailPage.routeName,
                   arguments: product.id);
             },
-            child: FadeInImage(
-              placeholder: AssetImage('assets/images/product-placeholder.png'),
-              image: NetworkImage(product.imageUrl),
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder:
+                    AssetImage('assets/images/product-placeholder.png'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
         ),
