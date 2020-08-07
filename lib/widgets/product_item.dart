7d6bf1b8196ef3cac80b124cb590e65b4612daaf
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-08-07 16:51:20 +0200

    feat: Added animation on product image loading

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -65,8 +65,9 @@ class ProductItem extends StatelessWidget {
               Navigator.of(context).pushNamed(ProductDetailPage.routeName,
                   arguments: product.id);
             },
-            child: Image.network(
-              product.imageUrl,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder.png'),
+              image: NetworkImage(product.imageUrl),
               fit: BoxFit.cover,
             ),
           ),
